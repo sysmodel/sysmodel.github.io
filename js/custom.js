3667835661760debc5ed2dfffd875fc59cfb5560
JosifFP <71727640+JosifFP@users.noreply.github.com>
2022-11-01 17:34:29 +0000

    Deploying to gh-pages from  @ a2374ed53fc3a78059a481e51417b683aaaa784a 🚀

--- js/custom.js
+++ js/custom.js
@@ -32,8 +32,14 @@
 				var $container = $('.isotope-container').isotope({
 					itemSelector: '.isotope-item',
 					layoutMode: 'masonry',
-					transitionDuration: '0.6s',
-					filter: "*"
+					transitionDuration: '0.4s',
+					resizable: false,
+					resizesContainer: true,
+					filter: "*",
+					masonry: {
+						isFitWidth: false 
+					  }
+					  
 				});
 				// filter items on button click
 				$('.filters').on( 'click', 'ul.nav li a', function() {
@@ -55,4 +61,77 @@
 		}
 
 	}); // End document ready
+
+})(this.jQuery);
+
+
+
+(function($){
+	$(document).ready(function(){
+
+		//EXTRA STUFF FOR ISOTOPES
+		if (jQuery.hasOwnProperty('Isotope')) {
+			jQuery.Isotope.prototype._getCenteredMasonryColumns = function() {
+			  this.width = this.element.width();
+			  
+			  var parentWidth = this.element.parent().width();
+			  
+							// i.e. options.masonry && options.masonry.columnWidth
+			  var colW = this.options.masonry && this.options.masonry.columnWidth ||
+							// or use the size of the first item
+							this.$filteredAtoms.outerWidth(true) ||
+							// if there's no items, use size of container
+							parentWidth;
+			  
+			  var cols = Math.floor( parentWidth / colW );
+			  cols = Math.max( cols, 1 );
+			  // i.e. this.masonry.cols = ....
+			  this.masonry.cols = cols;
+			  // i.e. this.masonry.columnWidth = ...
+			  this.masonry.columnWidth = colW;
+			};
+			
+			jQuery.Isotope.prototype._masonryReset = function() {
+			  // layout-specific props
+			  this.masonry = {};
+			  // FIXME shouldn't have to call this again
+			  this._getCenteredMasonryColumns();
+			  var i = this.masonry.cols;
+			  this.masonry.colYs = [];
+			  while (i--) {
+				this.masonry.colYs.push( 0 );
+			  }
+			};
+		  
+		  
+			jQuery.Isotope.prototype._masonryResizeChanged = function() {
+			  var prevColCount = this.masonry.cols;
+			  // get updated colCount
+			  this._getCenteredMasonryColumns();
+			  return ( this.masonry.cols !== prevColCount );
+			};
+			
+			jQuery.Isotope.prototype._masonryGetContainerSize = function() {
+			  var unusedCols = 0,
+				  i = this.masonry.cols;
+			  // count unused columns
+			  while ( --i ) {
+				if ( this.masonry.colYs[i] !== 0 ) {
+				  break;
+				}
+				unusedCols++;
+			  }
+			  
+			  return {
+					height : Math.max.apply( Math, this.masonry.colYs ),
+					// fit container to columns that have been used;
+					width : (this.masonry.cols - unusedCols) * this.masonry.columnWidth
+				  };
+			};
+		}
+
+		//EXTRA STUFF FOR ISOTOPES
+
+	}); // End document ready
+
 })(this.jQuery);
